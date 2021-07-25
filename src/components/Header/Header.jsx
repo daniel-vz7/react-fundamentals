b79af02fa3edc43c67c4d111392ded847dc5f805
206685300 <danieltux@outlook.com>
2021-07-24 22:59:10 -0600

    Update button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,31 @@
 import React from "react";
+import { useHistory } from "react-router";
+import axios from "axios";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import Logo from "../Logo/Logo";
 
 const Header = () => {
+  let history = useHistory();
+
+  const logout = async token => {
+    try {
+      return (await axios({
+        method: "delete",
+        url: "http://localhost:3000/logout",
+        headers: {
+          Authorization: token
+        }
+      })).data;
+    } catch (error) {
+      return {
+        successful: false,
+        message: error.message
+      }
+    }
+  }
+
   return (
     <Grid item container justifyContent="space-between" xs={11} direction="row" alignItems="center">
       <Grid item>
@@ -15,7 +36,28 @@ const Header = () => {
           <Typography>Daniel</Typography>
         </Grid>
         <Grid item>
-          <Button variant="contained" color="secondary">Logout</Button>
+          {window.localStorage.getItem('auth') !== null && (
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={async () => {
+                const removeReponse = await logout(window.localStorage.getItem('auth'));
+                if (removeReponse.successful) {
+                  window.localStorage.removeItem('auth');
+                  history.push('/login');
+                }
+              }}
+              >Logout</Button>
+          )}
+          {window.localStorage.getItem('auth') === null && (
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={() => {
+                history.push('/login');
+              }}
+              >Login</Button>
+          )}
         </Grid>
       </Grid>
     </Grid>
